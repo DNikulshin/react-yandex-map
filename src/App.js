0b7b9acbc1679984eb5b99b62b83ce21260f223b
dmnitprof <handleSearchQuery>
2022-09-25 08:16:53 +0300

    edit app

--- src/App.js
+++ src/App.js
@@ -2,26 +2,25 @@ import React, {useEffect, useRef} from 'react'
 import {Map, YMaps, SearchControl, Placemark, GeolocationControl} from 'react-yandex-maps'
 
 function App() {
-    const text = '55.46546, 37.546956'
-    const searchRef = useRef('')
+    const coordinate = '55.46546,37.546956'
+    const searchRef = useRef(null)
 
     useEffect(() => {
-        if (text && searchRef.current) {
-            searchRef.current.search(text)
+        if (coordinate && searchRef.current) {
+            searchRef.current = coordinate
+            console.log(searchRef.current)
         }
-        console.log(searchRef.current.search('55.46546,37.546956'))
-    }, [text])
+
+    }, [])
 
     return (
         <div className="map">
             <h2>Yandex Map</h2>
             <YMaps query={{apikey: '0c3c2c36-736e-4285-843a-2f4c3968c728'}}>
-                <Map defaultState={{center: [55.46546,37.546956], zoom: 9}} width="100vw" height="60vh">
-                    <SearchControl instanceRef={ref => {
-                     searchRef.current = ref
-                    }}/>
+                <Map defaultState={{center: [55.46546, 37.546956], zoom: 9}} width="100vw" height="60vh">
+                    <SearchControl instanceRef={searchRef.current = coordinate}/>
                     <Placemark
-                        geometry={[55.46546,37.546956]}
+                        geometry={[55.46546, 37.546956]}
                         properties={{}}
                     />
                     <GeolocationControl/>
